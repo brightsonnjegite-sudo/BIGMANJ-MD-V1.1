f67231a8ad6df1c449e5c2519a29e0a27a747109
BIGMANj <brightsonnjegite@gmail.com>
2026-06-08 17:54:05 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+module.exports = {
+    ownerNumber: process.env.OWNER_NUMBER || '255777580820',
+    botName: process.env.BOT_NAME || 'BIGMANj MD V1.1',
+    wm: '© bigmanj tech ™',
+    packname: 'BIGMANj MD V1.1',
+    author: '© bigmanj tech ™',
+    prefix: process.env.PREFIX || '.',
+    thumbnail: ['https://files.catbox.moe/x3wl8n.jpg'],
+    icon: ['https://files.catbox.moe/x3wl8n.jpg'],
+    mp41: 'https://example.com/video.mp4',
+    website: 'https://bigmanj.tech',
+    tele: 't.me/bigmanj_official',
+    ig: '@bigmanj_tech',
+    gcbot: 'https://chat.whatsapp.com/...',
+    version: '1.1',
+    botscript: 'https://github.com/brightsonnjegite-sudo/BIGMANJ-MD-V1.1',
+    qris: process.env.QRIS_URL || '',
+    dana: process.env.DANA || '',
+    gopay: process.env.GOPAY || '',
+    ovo: process.env.OVO || ''
+};
